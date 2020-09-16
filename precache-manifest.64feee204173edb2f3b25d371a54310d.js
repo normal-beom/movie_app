c69771b7709a9c8bf1c726b3ae996659a0f95dc8
dbskdlxlem13 <dbskdlxlem13@naver.com>
2020-09-16 15:46:32 +0900

    Updates

---
+++ precache-manifest.64feee204173edb2f3b25d371a54310d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416a1da27bd28c2935ff9fcf76ab6bf5",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cb12ea4b8656d53d9271",
+    "url": "/movie_app/static/css/main.3e669453.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb12ea4b8656d53d9271",
+    "url": "/movie_app/static/js/main.cfca2775.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
